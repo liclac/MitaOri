06e380198e3b3c8674d77593b16ee23c3d19881c
Johannes Ekberg <uppfinnarn@gmail.com>
2013-07-11 19:28:04 +0200

    Working Log module

--- modules/log/log.py
+++ modules/log/log.py
@@ -1,15 +1,18 @@
 import datetime
-from flask import Blueprint, render_template
+from flask import Blueprint, Response, render_template, jsonify, url_for
 from sqlalchemy import func, orm
 from models import *
 
 mod = Blueprint('log', __name__,
 				template_folder='templates')
 
+def get_dates():
+	query = db.session.query(func.DATE(Post.posted)).distinct()
+	return [ date[0] for date in query.all() ]
+
 @mod.route('/')
 def index():
-	query = db.session.query(func.DATE(Post.posted)).distinct()
-	dates = [ date[0] for date in query.all() ]
+	dates = get_dates()
 	return render_template('log/index.html', dates=dates)
 
 @mod.route('/<int:year>/<int:month>/<int:day>/')
@@ -20,4 +23,12 @@ def day(year, month, day):
 	# Failed attempt to preload characters, leaving for later
 	#character_ids = set(post.character_id for post in posts)
 	#list(Character.query.filter(Character.id.in_(character_ids)))
-	return render_template('log/day.html', date=date, posts=posts)
+	
+	back_date = db.session.query(func.DATE(Post.posted)).filter(Post.posted < date).order_by(Post.posted.desc()).first()[0]
+	next_date = db.session.query(func.DATE(Post.posted)).filter(Post.posted > (date + datetime.timedelta(days=1))).order_by(Post.posted).first()[0]
+	return render_template('log/day.html', date=date, posts=posts, back_date=back_date, next_date=next_date)
+
+@mod.route('/data/characters.css')
+def data_characters_css():
+	characters = Character.query.all()
+	return Response(render_template('log/characters.css', characters=characters), mimetype="text/css")
